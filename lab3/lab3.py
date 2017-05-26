3a2fee134ef447d588ddae24db1f7bdd59cf4c30
samibachir <sami.bachir@edu.esiee.fr>
2017-05-26 13:59:21 +0200

    new

---
+++ lab3/lab3.py
@@ -0,0 +1,153 @@
+import tweepy
+from tweepy import OAuthHandler
+
+
+consumer_key = 'byBuacZavrd0EjqLLtC3h4v2G'
+consumer_secret = 'SQbB73p3w4coR1GvPtERlGMTzc9FDqvwlj00to1Kqg2Zsl2gI4'
+access_token = '857901671044845569-kOe2ZpyeS70qYaZccebDbHCkH8roWHR'
+access_secret = 'iZkYMpJuOqITJDBt32FUNmeiacrrlPYwNGDMwUWI8l0L1'
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+user = api.me()
+
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+class MyListener(StreamListener):
+ 
+    def on_data(self, data):
+        try:
+            with open('tennis.json', 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+ 
+    def on_error(self, status):
+        print(status)
+        return True
+ 
+twitter_stream = Stream(auth, MyListener())
+
+#getting tweets talking about tennis and collecting them in a json file tennis.json
+twitter_stream.filter(track=['tennis'])
+
+import re
+ 
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+ 
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>', # HTML tags
+    r'(?:@[\w_]+)', # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)", # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', # URLs
+ 
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+    r'(?:[\w_]+)', # other words
+    r'(?:\S)' # anything else
+]
+    
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+ 
+def tokenize(s):
+    return tokens_re.findall(s)
+ 
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+
+
+import json 
+import tweepy
+
+json_file = open('tennis.json','r')
+
+#test to see if the json file was well created
+for line in json_file:
+ tweet = json.loads(line)
+ print (tweet['text'])
+
+
+import nltk
+from nltk.corpus import stopwords
+nltk.download("stopwords") # download the stopword corpus on our computer
+import string
+ 
+punctuation = list(string.punctuation)
+stop = stopwords.words('english') + punctuation + ['rt', 'via', 'RT']
+
+from collections import Counter
+import operator 
+name = 'tennis.json'
+
+with open(name, 'r') as f:
+    count_all = Counter()
+    for line in f:
+        tweet = json.loads(line)
+        # Create a list with all the terms
+        terms_hash = [term for term in preprocess(tweet['text']) if term.startswith('#') and term not in stop]        
+        count_all.update(terms_hash)
+# Print the first 10 most frequent words
+print(count_all.most_common(15))
+
+
+with open(name, 'r') as f:
+    count_all = Counter()
+    for line in f:
+        tweet = json.loads(line)
+        # Create a list with all the terms
+        terms_stop = [term for term in preprocess(tweet['text']) if term not in stop]
+        count_all.update(terms_stop)
+    for word, index in count_all.most_common(5):
+        print ('%s : %s' % (word, index))
+
+
+#treating the terms # and @ whose meanings are special in twitter
+terms_hash = [term for term in preprocess(tweet['text']) 
+              if term.startswith('#')]
+
+terms_only = [term for term in preprocess(tweet['text']) 
+              if term not in stop and
+              not term.startswith(('#', '@'))] 
+
+
+with open(name, 'r') as f:
+    count_all = Counter()
+    for line in f:
+        tweet = json.loads(line)
+        # Create a list with all the terms
+        terms_hash = [term for term in preprocess(tweet['text']) if term.startswith('#') and term not in stop]        
+        count_all.update(terms_hash)
+# Print the first 10 most frequent words
+print(count_all.most_common(15))
+
+
+
+#
+#  print a histogram of the most common words
+#
+
+%matplotlib inline
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (15,10)
+import matplotlib.pyplot as plt
+
+sorted_x, sorted_y = zip(*count_all.most_common(15))
+plt.bar(range(len(sorted_x)), sorted_y, width=0.75, align='center');
+plt.xticks(range(len(sorted_x)), sorted_x, rotation=80);
+plt.axis('tight'); 
